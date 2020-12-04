bab49b6b146aac62e3af8e0420521f63b5240253
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-05 08:27:44 +0900

    ランダムな数を出力させるようにする

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main 
 
 import "fmt"
-// import "math/rand"
-// import "time"
+import "math/rand"
+import "time"
 
 func main() {
+    rand.Seed(time.Now().Unix())
     for i := 1; i <= 3; i++ {
         fmt.Printf("%d回目のおみくじ結果: ", i)
     }
