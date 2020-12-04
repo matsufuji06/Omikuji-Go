7dc93c7c6dd69b8aa9c0fe1a39072b83f7f55ea4
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-05 08:14:49 +0900

    おみくじ結果をfor文で表示（仮）

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main 
 
 import "fmt"
-import "math/rand"
-import "time"
+// import "math/rand"
+// import "time"
 
 func main() {
-    
+    for i := 1; i <= 3; i++ {
+        fmt.Printf("%d回目のおみくじ結果: ", i)
+    }
 }
